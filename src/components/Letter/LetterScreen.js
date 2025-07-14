6f02118ea269714afe29c4be512aa7476dda30ee
HoangDuyPhucs <phuchd3112@gmail.com>
2025-07-14 23:36:41 +0700

    update content Letter

--- src/components/Letter/LetterScreen.js
+++ src/components/Letter/LetterScreen.js
@@ -10,7 +10,7 @@ function LetterScreen({ onNext }) {
           Hôm nay là ngày 15 tháng 7, chúc mừng sinh nhật em yêuuu nhé. Chúc em tuổi mới luôn thật nhiều sức khỏe, luôn mỉm cười rạng rỡ mỗi ngày, và giữ mãi sự hồn nhiên, đáng yêu của em.
         </p>
         <p>🎁 Cái này là món quà nhỏ a dành tặng cho e. a mới có ý tưởng và làm gần đây nên chắc chắn sẽ còn nhiều thiếu sót… Em thông cảm cho anh nhaaa!<br/> a sẽ cố gắng làm nó hoàn thiện. Ừm... chắc là deadline sẽ là đến lúc mình cưới nhé😆😆😆 .</p>
-        <p>Cảm ơn em đã ở bên anh khoảng 1 năm 1 tháng 15 ngày... anh đã rất vui, vì được đồng hành cùng e, cùng nhau lắng nghe, thấu hiểu nhau. Thời gian được ở canh e tuy không nhiều nhưng anh đều rất vui, yêu xa nhiều thiệt thòi nhỉ... nhưng cũng là thử thách để a trân trọng tình cảm của chúng mình hơn </p>
+        <p>Cảm ơn em đã yêu anh 1 năm 1 tháng 15 ngày... anh đã rất vui, vì được đồng hành cùng e, cùng nhau lắng nghe, thấu hiểu nhau. Thời gian được ở canh e tuy không nhiều nhưng anh đều rất vui, yêu xa nhiều thiệt thòi nhỉ... nhưng cũng là thử thách để a trân trọng tình cảm của chúng mình hơn </p>
         <p>Có lẽ, là 1 người hơi overlinhtinh, hay lo xa và nghĩ nhiều. Anh mong rằng, vào một ngày không xa,
           Em sẽ cho anh một “cái gật đầu” cho tương lai phía trước – nơi có anh, có em, và có cả chúng ta.</p>
         <p>
